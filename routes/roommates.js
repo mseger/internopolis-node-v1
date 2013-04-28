a5095b2d0e30c1c42f319dd33842ca44693f2aa4
Margaret-Ann Seger <margaret-ann@Margaret-Anns-MacBook-Air.local>
2013-04-28 16:06:50 -0400

    Getting friend list + friends' locations

--- routes/roommates.js
+++ routes/roommates.js
@@ -6,4 +6,15 @@ exports.displaySurvey = function(req, res){
 
 exports.calculateAndDisplayOptions = function(req, res){
 	// only display friends who actually fit the relevant criteria
+	var currUser = User.findOne({name: req.session.user.name}).exec(function (req2, user){
+		var friendList = user.friends; 
+		for(var i=0; i<friendList.length; i++){
+			req.facebook.api("/" + friendList[i].id + "?fields=id,name,location", function (err, friend){
+				if(err)
+					console.log("Error looking up friend: ", err);
+				console.log("FRIEND: ", friend.location);
+			});
+		}
+		//res.redirect('/roommates');
+	});
 }

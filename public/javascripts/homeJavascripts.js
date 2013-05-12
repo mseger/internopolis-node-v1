5ee3803059487ba0dbcd91c171c80b14375ff188
Margaret-Ann Seger <margaret-ann@Margaret-Anns-MacBook-Air.local>
2013-05-12 15:31:14 -0400

    Working email invites

--- public/javascripts/homeJavascripts.js
+++ public/javascripts/homeJavascripts.js
@@ -12,4 +12,10 @@ $(function () {
 
   $('#invite_modal').popup();
 
+  $('#fb_message_modal').popup();
+
+  $('.sendInviteEmail').on('submit', function () {
+    window.location.href = "mailto:" + $(this).serializeArray()[0].value + "?subject=Your%20friends%20want%20you%20to%20check%20out%20their%20group%20on%20Internopolis.%20To%20Internopolis&body=To%20join,%20check%20out%20http://internopolis.herokuapp.com/"+window.group;
+    return false;
+  })
 })
